59d28631e848d7aba2702f0936df41f6fbfcd3b8
shahana <shahanasherinwadi@gmail.com>
2025-05-07 15:33:26 +0530

    ui_component

---
+++ script.js
@@ -0,0 +1,26 @@
+const QUIZ_DATA = [
+    {
+        question: "wWich Language Runs In a Web Browser?",
+        options: ["java", "c", "python", "javascript"],
+        answer: "javascript"
+    },
+    {
+        question: "What Does CSS Stand For?",
+        options: ["central style sheet", "cascading style sheets", "cascading simple sheets",
+            "cars SUVs sailboats"
+        ],
+        answer: "cascading style sheets"
+    },
+    {
+        question: "What Does HTML Stands For?",
+        options: ["Hyper Text Markup Language", "Hyper Text Markdown Language",
+            "Hyper Loop Machiene Language", "Helicopters Terminals Motorboats Lamborginis"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "What Year Was Javascript Launched?",
+        Options: ["1996", "1995", "1994", "none of the above"],
+        answer: "1995"
+    }
+]
